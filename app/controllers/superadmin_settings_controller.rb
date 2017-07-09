e54909fb4d19b60da23a1df9d19010dba3f2a597
Karan Vaid <vaidkaran89@gmail.com>
2017-07-09 18:54:49 +0530

    added a success key to the json returned by superadmin_settings controller actions

--- app/controllers/superadmin_settings_controller.rb
+++ app/controllers/superadmin_settings_controller.rb
@@ -5,12 +5,12 @@ class SuperadminSettingsController < ApplicationController
   def create_superadmin
     user = User.find_by(email: params[:email])
     if user.nil?
-      render json: {error: "Could not find an existing user with the email: #{params[:email]}"}
+      render json: {success: 'false', error: "Could not find an existing user with the email: #{params[:email]}"}
     else
       if user.update(superadmin: true)
-        render json: {message: "User with email: #{params[:email]} is now a superadmin}"}
+        render json: {success: 'true', message: "User with email: #{params[:email]} is now a superadmin}"}
       else
-        render json: {error: "Something went wrong. Could not create superadmin."}
+        render json: {success: 'false', error: "Something went wrong. Could not create superadmin."}
       end
     end
   end
@@ -18,12 +18,12 @@ class SuperadminSettingsController < ApplicationController
   def destroy_superadmin
     user = User.find_by(email: params[:email])
     if user.nil?
-      render json: {error: "Could not find an existing user with the email: #{params[:email]}"}
+      render json: {success: 'false', error: "Could not find an existing user with the email: #{params[:email]}"}
     else
       if user.update(superadmin: false)
-        render json: {message: "User with email: #{params[:email]} is no longer a superadmin}"}
+        render json: {success: 'true', message: "User with email: #{params[:email]} is no longer a superadmin}"}
       else
-        render json: {error: "Something went wrong. Could not destroy superadmin."}
+        render json: {success: 'false', error: "Something went wrong. Could not destroy superadmin."}
       end
     end
   end
